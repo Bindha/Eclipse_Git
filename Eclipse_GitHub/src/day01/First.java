38f77d4024054e740869078fada76b0561132df7
Bindha <85977947+Bindha@users.noreply.github.com>
2021-06-18 22:01:57 +0530

    Update First.java

--- Eclipse_GitHub/src/day01/First.java
+++ Eclipse_GitHub/src/day01/First.java
@@ -4,7 +4,7 @@ public class First {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("Testing!!!!1");
+         System.out.println("Brindha testing");
 	}
 
 }
